163f949c96a4feadaeab20a3f4b9c340ed2194aa
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-26 09:01:05 +0700

    update create user request

--- requests/create_user.requests.go
+++ requests/create_user.requests.go
@@ -4,9 +4,9 @@ type CreateUserRequest struct {
 	Name      string `json:"name" binding:"required,min=3"`
 	Email     string `json:"email" binding:"required,email"`
 	Username  string `json:"username" binding:"required,min=3,alphanum"`
-	Password  string `json:"passsword" binding:"required,password"`
+	Password  string `json:"password" binding:"required,password,min=8"`
 	Role      string `json:"role" binding:"omitempty,oneof=USER ADMIN"`
-	Status    string `json:"status" binding:"omitempty,oneof=0 1"`
+	Status    int    `json:"status" binding:"omitempty,oneof=0 1"`
 	BirthDate string `json:"birthDate" binding:"omitempty,birthdate"`
 	Phone     string `json:"phone" binding:"omitempty,numeric,min=10,max=15"`
 	Gender    string `json:"gender" binding:"omitempty,oneof=male female"`
