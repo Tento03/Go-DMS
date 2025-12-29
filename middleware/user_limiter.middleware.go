44b5ef3c8c6dce7bfb087edf885be7057136328c
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-29 22:05:03 +0700

    add create user limiter in user_limiter.middleware.go

---
+++ middleware/user_limiter.middleware.go
@@ -0,0 +1,41 @@
+package middleware
+
+import (
+	"fmt"
+	"go-dms/config"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateUserLimiter(maxAttempt int, window time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		userId := c.GetUint("userId")
+		key := fmt.Sprintf("rl:create:%s:%d", ip, userId)
+
+		count, err := config.Client.Incr(config.Ctx, key).Result()
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": "redis error",
+			})
+			return
+		}
+
+		if count == 1 {
+			config.Client.Expire(config.Ctx, key, window)
+		}
+
+		if count > int64(maxAttempt) {
+			ttl, _ := config.Client.TTL(config.Ctx, key).Result()
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error":        "too many create user requests",
+				"retry_at":     int(ttl.Seconds()),
+				"max_attempts": maxAttempt,
+			})
+			return
+		}
+		c.Next()
+	}
+}
