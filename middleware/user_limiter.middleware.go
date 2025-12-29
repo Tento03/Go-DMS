ca2119c0bdd49dff9f351603c9c0fc658191e1d9
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-29 22:18:24 +0700

    add update user limiter

--- middleware/user_limiter.middleware.go
+++ middleware/user_limiter.middleware.go
@@ -39,3 +39,34 @@ func CreateUserLimiter(maxAttempt int, window time.Duration) gin.HandlerFunc {
 		c.Next()
 	}
 }
+
+func UpdateUserLimiter(maxAttempt int, window time.Duration) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		ip := c.ClientIP()
+		userId := c.GetUint("userId")
+		key := fmt.Sprintf("rl:update:%s:%d", ip, userId)
+
+		count, err := config.Client.Incr(config.Ctx, key).Result()
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"error": "redis error",
+			})
+			return
+		}
+
+		if count == 1 {
+			config.Client.Expire(config.Ctx, key, window)
+		}
+
+		if count > int64(maxAttempt) {
+			ttl, _ := config.Client.TTL(config.Ctx, key).Result()
+			c.AbortWithStatusJSON(http.StatusTooManyRequests, gin.H{
+				"error":        "too many update user requests",
+				"retry_after":  int(ttl.Seconds()),
+				"max_attempts": maxAttempt,
+			})
+			return
+		}
+		c.Next()
+	}
+}
