cf8a4574defb115ec70b7ff2adfa1e3a13e23488
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2025-12-29 23:10:11 +0700

    update routes

--- routes/user.routes.go
+++ routes/user.routes.go
@@ -2,16 +2,19 @@ package routes
 
 import (
 	"go-dms/controllers"
+	"go-dms/middleware"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRoutes(r *gin.Engine) {
-	r.GET("/users", controllers.GetAll)
-	r.GET("/users/:id", controllers.GetById)
-	r.POST("/users", controllers.Create)
-	r.PUT("/users/:id", controllers.Update)
-	r.DELETE("/users/:id", controllers.Delete)
-	r.PATCH("users/reset-password/:id", controllers.ResetPassword)
-	r.PATCH("/users/change-password/:id", controllers.ChangePassword)
+	user := r.Group("/users")
+	user.GET("/", controllers.GetAll)
+	user.GET("/:id", controllers.GetById)
+	user.POST("/", middleware.CreateUserLimiter(5, 2*time.Minute), controllers.Create)
+	user.PUT("/:id", middleware.UpdateUserLimiter(5, 2*time.Minute), controllers.Update)
+	user.DELETE("/:id", middleware.DeleteUserLimiter(5, 2*time.Minute), controllers.Delete)
+	user.PATCH("users/reset-password/:id", controllers.ResetPassword)
+	user.PATCH("/users/change-password/:id", controllers.ChangePassword)
 }
