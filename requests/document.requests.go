101656a9c2fdec0a13080ea6e2fb372ab0edc939
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-03 17:01:07 +0700

    add document.requests.go

---
+++ requests/document.requests.go
@@ -0,0 +1,7 @@
+package requests
+
+type CreateDocumentRequest struct {
+	Title       string `form:"title" binding:"required,min=3,max=100"`
+	Description string `form:"description" binding:"max=255"`
+	Type        string `form:"type" binding:"required,oneof=pdf image docx"`
+}
