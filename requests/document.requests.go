685a6a8f2aa9b32107935fc3eb97081a5fcef03c
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-03 17:18:51 +0700

    update document request

--- requests/document.requests.go
+++ requests/document.requests.go
@@ -2,6 +2,6 @@ package requests
 
 type CreateDocumentRequest struct {
 	Title       string `form:"title" binding:"required,min=3,max=100"`
-	Description string `form:"description" binding:"max=255"`
+	Description string `form:"description" binding:"required,max=255"`
 	Type        string `form:"type" binding:"required,oneof=pdf image docx"`
 }
