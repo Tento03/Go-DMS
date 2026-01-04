f419351246c6db534ff69aab6f41cb635eab8dc6
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-04 21:04:37 +0700

    add create document func in document controller and update UserId data type in document model

---
+++ controllers/document.controller.go
@@ -0,0 +1,85 @@
+package controllers
+
+import (
+	"fmt"
+	"go-dms/config"
+	"go-dms/models"
+	"go-dms/requests"
+	"net/http"
+	"path/filepath"
+	"slices"
+	"strings"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CreateDocument(c *gin.Context) {
+	//1. Bind JSON
+	var req requests.CreateDocumentRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"errors": err.Error()})
+		return
+	}
+
+	//2. Ambil dari form
+	file, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file is required"})
+		return
+	}
+
+	//3. Validasi ukuran file
+	if file.Size > 5<<20 {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file size max 5mb"})
+		return
+	}
+
+	//4. Validasi extensi file
+	ext := strings.ToLower(filepath.Ext(file.Filename))
+	allowed := map[string][]string{
+		"pdf":   {".pdf"},
+		"image": {".jpg", ".jpeg", ".png"},
+		"docx":  {".docx"},
+	}
+
+	if !slices.Contains(allowed[req.Type], ext) {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "file type not allowed"})
+		return
+	}
+
+	//5. Ambil UserId
+	userId := c.GetUint("userId")
+
+	//6. Generate nama file aman
+	filename := fmt.Sprintf(
+		"%d_%d%s",
+		userId,
+		time.Now().UnixNano(),
+		ext,
+	)
+
+	path := filepath.Join("uploads", filename)
+
+	//7. Simpan file
+	if err := c.SaveUploadedFile(file, path); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save file"})
+		return
+	}
+
+	//8. Simpan ke DB
+	doc := models.Document{
+		UserID:      userId,
+		Title:       req.Title,
+		Description: req.Description,
+		Type:        req.Type,
+		Path:        path,
+	}
+	if err := config.DB.Create(&doc).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "failed to save document"})
+		return
+	}
+
+	//9. Hasilkan response
+	c.JSON(http.StatusCreated, gin.H{"message": "document uploaded", "data": doc})
+}
--- models/document.model.go
+++ models/document.model.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Document struct {
 	gorm.Model
-	UserID      string `gorm:"not null"`
+	UserID      uint   `gorm:"not null"`
 	Title       string `gorm:"size:100;not null"`
 	Description string `gorm:"size:255;not null"`
 	Type        string `gorm:"size:20;not null"`
