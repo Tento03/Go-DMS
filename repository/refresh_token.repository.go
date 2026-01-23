3789d1a585a655ecf48459691a9fb8feccfcc87b
CHRISTENTO SIREGAR <106148417+Tento03@users.noreply.github.com>
2026-01-23 22:47:07 +0700

    add refresh token repo

---
+++ repository/refresh_token.repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"go-dms/config"
+	"go-dms/models"
+	"time"
+)
+
+func SaveRefreshToken(refresh *models.Refresh) error {
+	return config.DB.Create(refresh).Error
+}
+
+func FindValidRefreshToken(token string) (*models.Refresh, error) {
+	var refresh models.Refresh
+	err := config.DB.Model(&models.Refresh{}).Where("token = ? AND expires_at IS NULL", token).First(refresh).Error
+	return &refresh, err
+}
+
+func RevokeAllUser(userId string) error {
+	now := time.Now()
+	return config.DB.Model(&models.Refresh{}).Where("user_id = ?", userId).UpdateColumn("revoked_at", &now).Error
+}
+
+func RevokeToken(refresh *models.Refresh) error {
+	now := time.Now()
+	return config.DB.Model(refresh).UpdateColumn("revoked_at", &now).Error
+}
